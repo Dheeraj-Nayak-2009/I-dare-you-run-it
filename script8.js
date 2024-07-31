a532c006b604549190347a32381309d35125a534
Dheeraj Nayak <ddnayak103@gmail.com>
2024-07-31 22:43:46 +0530

    script8.js

---
+++ script8.js
@@ -0,0 +1,13 @@
+document.getElementById('eggForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const response = document.activeElement.value; // Get the value of the clicked button
+
+    if (response === 'Make an omelette. \uD83C\uDF73') { // Unicode for 🍳
+        this.action = 'index9.html';
+    } else if (response === 'Hatch it. \uD83D\uDC23') { // Unicode for 🐣
+        this.action = 'index10.html';
+    }
+
+    this.submit(); // Submit the form with the new action
+});
